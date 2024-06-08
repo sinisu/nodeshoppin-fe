035caf881eaefea29d48d4f8b5f645f3b9221bcf
yisoo song <yisoo1049@gmail.com>
2024-06-09 02:10:20 +0900

    cartreducer

--- src/action/cartAction.js
+++ src/action/cartAction.js
@@ -1,9 +1,18 @@
 import api from "../utils/api";
 import * as types from "../constants/cart.constants";
 import { commonUiActions } from "../action/commonUiAction";
-const addToCart =
-  ({ id, size }) =>
-  async (dispatch) => {};
+const addToCart = ({ id, size }) => async (dispatch) => {
+  try{
+    dispatch({type:types.ADD_TO_CART_REQUEST});
+    const response = await api.post("/cart",{productId:id,size,qty:1});
+    if (response.status !== 200) throw new Error(response.error);
+    dispatch({type:types.ADD_TO_CART_SUCCESS,payload:response.data}); //TODO
+    commonUiActions.showToastMessage("카트에 상품이 추가되었습니다.","success");
+  }catch(error){
+    dispatch({type:types.ADD_TO_CART_FAIL,payload:error.error});
+    commonUiActions.showToastMessage(error.error,"error");
+  };
+};
 
 const getCartList = () => async (dispatch) => {};
 const deleteCartItem = (id) => async (dispatch) => {};
--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -14,6 +14,7 @@ const ProductDetail = () => {
 
   const [size, setSize] = useState("");
   const { id } = useParams();
+  const {user} = useSelector((state)=>state.user);
   const [sizeError, setSizeError] = useState(false);
   const product = useSelector((state)=>state.product);
   const selectedProduct = useSelector((state)=>state.product.productItem);
@@ -22,11 +23,19 @@ const ProductDetail = () => {
   const navigate = useNavigate();
   const addItemToCart = () => {
     //사이즈를 아직 선택안했다면 에러
+    if(size === '') {
+      setSizeError(true);
+      return
+    }
     // 아직 로그인을 안한유저라면 로그인페이지로
+    if(!user) navigate('/login');
     // 카트에 아이템 추가하기
+    dispatch(cartActions.addToCart({id,size}));
   };
   const selectSize = (value) => {
     // 사이즈 추가하기
+    if(sizeError) setSizeError(false);
+    setSize(value);
   };
 
   //카트에러가 있으면 에러메세지 보여주기
